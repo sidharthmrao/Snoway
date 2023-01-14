c2dcd0e2192ee31d36428c045227bcd06789c671
Sidharth Rao <sidharthmrao@gmail.com>
2023-01-13 21:47:29 -0500

    Created login, signup, and profile functions.

--- main.py
+++ main.py
@@ -1,15 +1,71 @@
 from flask import Flask, request
-from markupsafe import escape
-
 from mongo_utils import *
 
 app = Flask(__name__)
-controller = MongoController()
+db_controller = MongoController()
 
-controller.display_db()
 
-@app.route("/login")
-def hello_world():
-    username = request.args.get('username')
-    password = request.args.get('password')
-    return f"{verify_auth(escape(username), escape(password))}"
+@app.route("/login", methods=["POST"])
+def login():
+    args = request.get_json()
+    username = args["username"]
+    password = args["password"]
+
+    user = db_controller.verify_auth(username, password)
+
+    if user:
+        return {
+            "status": "success",
+            "message": "Successfully logged in.",
+            "uuid": user["uuid"],
+            "username": user["username"],
+            "full_name": user["full_name"],
+            "email": user["email"],
+            "bio": user["bio"],
+        }, 200
+    else:
+        return {
+            "status": "failure",
+        }, 401
+
+
+@app.route("/signup", methods=["POST"])
+def sign_up():
+    args = request.get_json()
+    username = args["username"]
+    password = args["password"]
+    full_name = args["full_name"]
+    email = args['email']
+    try:
+        db_controller.add_user(username, password, full_name, email)
+        return {
+            "status": "success",
+            "message": "Successfully signed up."
+        }, 200
+    except Exception as e:
+        return {
+            "status": "failure",
+        }, 500
+
+
+@app.route("/profile", methods=["POST"])
+def get_user_data():
+    args = request.get_json()
+    user_uuid = args["uuid"]
+
+    user = db_controller.get_user(user_uuid)
+
+    if user:
+        return {
+            "status": "success",
+            "message": "User found.",
+            "uuid": user["uuid"],
+            "username": user["username"],
+            "full_name": user["full_name"],
+            "email": user["email"],
+            "bio": user["bio"],
+        }, 200
+    else:
+        return {
+            "status": "failure",
+        }, 401
--- mongo_utils.py
+++ mongo_utils.py
@@ -1,14 +1,43 @@
 import pymongo
+import uuid
 
 
 class MongoController:
     def __init__(self):
         self.client = pymongo.MongoClient(
             "mongodb+srv://snowday:snowday@snowday.nktu0z5.mongodb.net/?retryWrites=true&w=majority")
-        self.db = self.client.list_database_names()
+        self.db = self.client["SnowDay"]
 
-    def display_db(self):
-        print(self.db)
+        self.users = self.db["Users"]
+
+    def add_user(self, username: str, password: str, full_name: str, email: str):
+        self.users.insert_one(
+            {
+                "username": username,
+                "password": password,
+                "full_name": full_name,
+                "email": email,
+                "bio": "",
+                "uuid": str(uuid.uuid4())
+            }
+        )
+
+    def get_user(self, user_uuid: str):
+        user = self.users.find_one(
+            {"uuid": user_uuid}
+        )
+
+        return user
+
+    def verify_auth(self, username, password):
+        user = self.users.find_one({"username": username})
+
+        if user is None:
+            return False
+        elif user["password"] != password:
+            return False
+        else:
+            return user
 
 
 def verify_auth(username, password):
