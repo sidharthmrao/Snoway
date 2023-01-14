679d7e21e03eda64889088abd84f412fc2aa8522
Sidharth Rao <sidharthmrao@gmail.com>
2023-01-14 00:34:52 -0500

    Added Location/Review support, integrated with user. Added more endpoints.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from mongo_utils import *
 
 app = Flask(__name__)
-user_controller = UserController()
+user_controller = MongoController()
 
 
 @app.route("/login", methods=["POST"])
@@ -14,29 +14,30 @@ def login():
         password = args["password"]
     except:
         return {
-            "status": "failure",
-            "message": "Missing or invalid arguments."
-        }, 400
+                   "status": "failure",
+                   "message": "Missing or invalid arguments."
+               }, 400
 
     user = user_controller.verify_auth(username, password)
 
     if user:
         return {
-            "status": "success",
-            "message": "Successfully logged in.",
-            "uuid": user["uuid"],
-            "username": user["username"],
-            "full_name": user["full_name"],
-            "email": user["email"],
-            "bio": user["bio"],
-            "city": user["city"],
-            "reviews": user["reviews"]
-        }, 200
+                   "status": "success",
+                   "message": "Successfully logged in.",
+                   "uuid": user["uuid"],
+                   "username": user["username"],
+                   "full_name": user["full_name"],
+                   "email": user["email"],
+                   "bio": user["bio"],
+                   "city": user["city"],
+                   "reviews": user["reviews"],
+                   "locations": user["locations"]
+               }, 200
     else:
         return {
-            "status": "failure",
-            "message": "Invalid credentials."
-        }, 401
+                   "status": "failure",
+                   "message": "Invalid credentials."
+               }, 401
 
 
 @app.route("/signup", methods=["POST"])
@@ -51,28 +52,34 @@ def sign_up():
         city = args["city"]
     except Exception as e:
         return {
-            "status": "failure",
-            "message": "Missing required arguments."
-        }, 400
+                   "status": "failure",
+                   "message": "Missing required arguments."
+               }, 400
 
     try:
         user_resp = user_controller.add_user(username, password, full_name, email, city)
 
         if user_resp == "Username already exists.":
             return {
-                "status": "failure",
-                "message": user_resp
-            }, 400
-
-        return {
-            "status": "success",
-            "message": "Successfully signed up."
-        }, 200
+                       "status": "failure",
+                       "message": user_resp
+                   }, 400
+        elif user_resp == "Email already exists.":
+            return {
+                       "status": "failure",
+                       "message": user_resp
+                   }, 400
+        else:
+            return {
+                       "status": "success",
+                       "message": "Successfully signed up.",
+                       "uuid": user_resp,
+                   }, 200
 
     except Exception as e:
         return {
-            "status": "failure",
-        }, 500
+                   "status": "failure",
+               }, 500
 
 
 @app.route("/profile", methods=["POST"])
@@ -83,28 +90,29 @@ def get_user_data():
         user_uuid = args["uuid"]
     except Exception as e:
         return {
-            "status": "failure",
-            "message": "Missing required arguments."
-        }, 400
+                   "status": "failure",
+                   "message": "Missing required arguments."
+               }, 400
 
     user = user_controller.get_user(user_uuid)
 
     if user:
         return {
-            "status": "success",
-            "message": "User found.",
-            "uuid": user["uuid"],
-            "username": user["username"],
-            "full_name": user["full_name"],
-            "email": user["email"],
-            "bio": user["bio"],
-            "city": user["city"],
-            "reviews": user["reviews"]
-        }, 200
+                   "status": "success",
+                   "message": "User found.",
+                   "uuid": user["uuid"],
+                   "username": user["username"],
+                   "full_name": user["full_name"],
+                   "email": user["email"],
+                   "bio": user["bio"],
+                   "city": user["city"],
+                   "reviews": user["reviews"],
+                   "locations": user["locations"]
+               }, 200
     else:
         return {
-            "status": "failure",
-        }, 401
+                   "status": "failure",
+               }, 401
 
 
 @app.route("/edit_user", methods=["POST"])
@@ -118,17 +126,162 @@ def edit_user_data():
         new_data = args
     except Exception as e:
         return {
-            "status": "failure",
-            "message": "Missing required arguments."
-        }, 400
+                   "status": "failure",
+                   "message": "Missing required arguments."
+               }, 400
 
     if user_controller.edit_user(user_uuid, new_data):
         return {
-            "status": "success",
-            "message": "User found.",
-        }, 200
+                   "status": "success",
+                   "message": "User found.",
+               }, 200
     else:
         return {
-            "status": "failure",
-        }, 401
+                   "status": "failure",
+               }, 401
 
+
+@app.route("/add_location", methods=["POST"])
+def add_location():
+    args = request.get_json()
+
+    try:
+        location_coords = args["location_coords"]
+        location_name = args["location_name"]
+        location_description = args["location_description"]
+        location_type = args["location_type"]
+        location_image = args["location_image"]
+        user_uuid = args["user_uuid"]
+    except Exception as e:
+        return {
+                   "status": "failure",
+                   "message": "Missing required arguments."
+               }, 400
+
+    location_response = user_controller.add_location(location_coords, location_name, location_description,
+                                                     location_type, location_image, user_uuid)
+
+    if location_response and location_response != "Location already exists.":
+        return {
+                   "status": "success",
+                   "message": "Location added.",
+                   "uuid": location_response,
+               }, 200
+    elif location_response == "Location already exists.":
+        return {
+                   "status": "failure",
+                   "message": "Location already exists."
+               }, 400
+    else:
+        return {
+                   "status": "failure",
+               }, 401
+
+
+@app.route("/get_location", methods=["POST"])
+def get_location():
+    args = request.get_json()
+    try:
+        location_uuid = args["uuid"]
+    except Exception as e:
+        return {
+                   "status": "failure",
+                   "message": "Missing required arguments."
+               }, 400
+
+    location = user_controller.get_location(location_uuid)
+
+    if location:
+        return {
+                   "status": "success",
+                   "message": "Location found.",
+                   "uuid": location["uuid"],
+                   "location_coords": location["location_coords"],
+                   "location_name": location["location_name"],
+                   "location_description": location["location_description"],
+                   "location_type": location["location_type"],
+                   "location_image": location["location_image"],
+                   "location_reviews": location["location_reviews"],
+                   "user_uuid": location["user_uuid"]
+               }, 200
+    else:
+        return {
+                   "status": "failure",
+                   "message": "Location not found."
+               }, 401
+
+
+@app.route("/add_review", methods=["POST"])
+def add_review():
+    args = request.get_json()
+
+    try:
+        location_uuid = args["location_uuid"]
+        user_uuid = args["user_uuid"]
+        review_description = args["review_description"]
+        review_rating = args["review_rating"]
+        picture = args["picture"]
+    except Exception as e:
+        return {
+                   "status": "failure",
+                   "message": "Missing required arguments."
+               }, 400
+
+    review_response = user_controller.add_review(location_uuid, user_uuid, review_description, review_rating, picture)
+
+    if review_response == "Location not found.":
+        return {
+                   "status": "failure",
+                   "message": "Location not found."
+               }, 400
+    elif review_response == "User not found.":
+        return {
+                   "status": "failure",
+                   "message": "User not found."
+               }, 400
+    elif review_response == "User already reviewed this location.":
+        return {
+                   "status": "failure",
+                   "message": "User already reviewed this location."
+               }, 400
+    elif review_response:
+        return {
+                   "status": "success",
+                   "message": "Review added.",
+                   "uuid": review_response,
+               }, 200
+    else:
+        return {
+                   "status": "failure",
+               }, 401
+
+
+@app.route("/get_review", methods=["POST"])
+def get_review():
+    args = request.get_json()
+    try:
+        review_uuid = args["uuid"]
+    except Exception as e:
+        return {
+                   "status": "failure",
+                   "message": "Missing required arguments."
+               }, 400
+
+    review = user_controller.get_review(review_uuid)
+
+    if review:
+        return {
+                   "status": "success",
+                   "message": "Review found.",
+                   "uuid": review["uuid"],
+                   "location_uuid": review["location_uuid"],
+                   "user_uuid": review["user_uuid"],
+                   "review_description": review["review_description"],
+                   "review_rating": review["review_rating"],
+                   "picture": review["picture"]
+               }, 200
+    else:
+        return {
+                   "status": "failure",
+                   "message": "Review not found."
+               }, 401
--- mongo_utils.py
+++ mongo_utils.py
@@ -2,24 +2,27 @@ import pymongo
 import uuid
 
 
-class UserController:
+class MongoController:
     def __init__(self):
         self.client = pymongo.MongoClient(
             "mongodb+srv://snowday:snowday@snowday.nktu0z5.mongodb.net/?retryWrites=true&w=majority")
         self.db = self.client["SnowDay"]
         self.users = self.db["Users"]
+        self.locations = self.db["Locations"]
+        self.reviews = self.db["Reviews"]
 
     def add_user(self, username: str, password: str, full_name: str, email: str, city: str):
         user = self.users.find_one({"username": username})
         if user:
             return "Username already exists."
 
-        user = self.users.find_one({"email": email})
-
+        user = self.users.find_one({"email": str(email).lower()})
         if user:
             return "Email already exists."
 
-        return self.users.insert_one(
+        user_uuid = str(uuid.uuid4())
+
+        self.users.insert_one(
             {
                 "username": username,
                 "password": password,
@@ -27,10 +30,13 @@ class UserController:
                 "email": email,
                 "bio": "",
                 "reviews": [],
+                "locations": [],
                 "city": city,
-                "uuid": str(uuid.uuid4())
+                "uuid": user_uuid
             }
         )
+
+        return user_uuid
 
     def get_user(self, user_uuid: str):
         user = self.users.find_one(
@@ -73,3 +79,92 @@ class UserController:
             return False
         else:
             return user
+
+    ######## LOCATION ########
+    def add_location(self, location_coords, location_name, location_description, location_type, location_image, user_uuid):
+        location = self.locations.find_one({"location_coords": location_coords})
+        if location:
+            return "Location already exists."
+
+        user = self.users.find_one({"uuid": user_uuid})
+
+        if not user:
+            return "User not found."
+
+        location_uuid = str(uuid.uuid4())
+
+        self.locations.insert_one(
+            {
+                "uuid": location_uuid,
+                "location_coords": location_coords,
+                "location_name": location_name,
+                "location_description": location_description,
+                "location_type": location_type,
+                "location_image": location_image,
+                "user_uuid": user_uuid,
+                "location_reviews": []
+            }
+        )
+
+        self.users.update_one(
+            {"uuid": user_uuid},
+            {"$push": {"locations": location_uuid}}
+        )
+
+        return location_uuid
+
+    def get_location(self, location_uuid: str):
+        location = self.locations.find_one(
+            {"uuid": location_uuid}
+        )
+
+        return location
+
+    def add_review(self, location_uuid: str, user_uuid: str, review_description: str, review_rating: str,  picture: str) -> object:
+        review_uuid = str(uuid.uuid4())
+
+        description = {
+            "uuid": review_uuid,
+            "review_description": review_description,
+            "review_rating": review_rating,
+            "user_uuid": user_uuid,
+            "location_uuid": location_uuid,
+            "picture": picture
+        }
+
+        location = self.locations.find_one({"uuid": location_uuid})
+
+        if not location:
+            return "Location not found."
+
+        user = self.users.find_one({"uuid": user_uuid})
+
+        if not user:
+            return "User not found."
+
+        location_reviews = self.locations.find_one({"uuid": location_uuid})["location_reviews"]
+
+        for review in location_reviews:
+            if self.reviews.find_one({"uuid": review})["user_uuid"] == user_uuid:
+                return "User already reviewed this location."
+
+        self.locations.update_one(
+            {"uuid": location_uuid},
+            {"$push": {"location_reviews": description["uuid"]}}
+        )
+
+        self.users.update_one(
+            {"uuid": user_uuid},
+            {"$push": {"reviews": description["uuid"]}}
+        )
+
+        self.reviews.insert_one(description)
+
+        return review_uuid
+
+    def get_review(self, review_uuid: str):
+        review = self.reviews.find_one(
+            {"uuid": review_uuid}
+        )
+
+        return review
