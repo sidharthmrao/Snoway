4e53ad93fd2430d3e0b12a18a79657eb84bd16ad
Sidharth Rao <sidharthmrao@gmail.com>
2023-01-13 22:34:34 -0500

    Working user and login system.

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ from flask import Flask, request
 from mongo_utils import *
 
 app = Flask(__name__)
-db_controller = MongoController()
+user_controller = UserController()
 
 
 @app.route("/login", methods=["POST"])
 def login():
     args = request.get_json()
-    username = args["username"]
-    password = args["password"]
 
-    user = db_controller.verify_auth(username, password)
+    try:
+        username = args["username"]
+        password = args["password"]
+    except:
+        return {
+            "status": "failure",
+            "message": "Missing or invalid arguments."
+        }, 400
+
+    user = user_controller.verify_auth(username, password)
 
     if user:
         return {
@@ -22,26 +29,46 @@ def login():
             "full_name": user["full_name"],
             "email": user["email"],
             "bio": user["bio"],
+            "city": user["city"],
+            "reviews": user["reviews"]
         }, 200
     else:
         return {
             "status": "failure",
+            "message": "Invalid credentials."
         }, 401
 
 
 @app.route("/signup", methods=["POST"])
 def sign_up():
     args = request.get_json()
-    username = args["username"]
-    password = args["password"]
-    full_name = args["full_name"]
-    email = args['email']
+
     try:
-        db_controller.add_user(username, password, full_name, email)
+        username = args["username"]
+        password = args["password"]
+        full_name = args["full_name"]
+        email = str(args['email']).lower()
+        city = args["city"]
+    except Exception as e:
+        return {
+            "status": "failure",
+            "message": "Missing required arguments."
+        }, 400
+
+    try:
+        user_resp = user_controller.add_user(username, password, full_name, email, city)
+
+        if user_resp == "Username already exists.":
+            return {
+                "status": "failure",
+                "message": user_resp
+            }, 400
+
         return {
             "status": "success",
             "message": "Successfully signed up."
         }, 200
+
     except Exception as e:
         return {
             "status": "failure",
@@ -51,9 +78,16 @@ def sign_up():
 @app.route("/profile", methods=["POST"])
 def get_user_data():
     args = request.get_json()
-    user_uuid = args["uuid"]
 
-    user = db_controller.get_user(user_uuid)
+    try:
+        user_uuid = args["uuid"]
+    except Exception as e:
+        return {
+            "status": "failure",
+            "message": "Missing required arguments."
+        }, 400
+
+    user = user_controller.get_user(user_uuid)
 
     if user:
         return {
@@ -64,8 +98,37 @@ def get_user_data():
             "full_name": user["full_name"],
             "email": user["email"],
             "bio": user["bio"],
+            "city": user["city"],
+            "reviews": user["reviews"]
         }, 200
     else:
         return {
             "status": "failure",
         }, 401
+
+
+@app.route("/edit_user", methods=["POST"])
+def edit_user_data():
+    args = request.get_json()
+
+    try:
+        user_uuid = args["uuid"]
+        args = dict(args)
+        args.pop("uuid")
+        new_data = args
+    except Exception as e:
+        return {
+            "status": "failure",
+            "message": "Missing required arguments."
+        }, 400
+
+    if user_controller.edit_user(user_uuid, new_data):
+        return {
+            "status": "success",
+            "message": "User found.",
+        }, 200
+    else:
+        return {
+            "status": "failure",
+        }, 401
+
--- mongo_utils.py
+++ mongo_utils.py
@@ -2,22 +2,32 @@ import pymongo
 import uuid
 
 
-class MongoController:
+class UserController:
     def __init__(self):
         self.client = pymongo.MongoClient(
             "mongodb+srv://snowday:snowday@snowday.nktu0z5.mongodb.net/?retryWrites=true&w=majority")
         self.db = self.client["SnowDay"]
-
         self.users = self.db["Users"]
 
-    def add_user(self, username: str, password: str, full_name: str, email: str):
-        self.users.insert_one(
+    def add_user(self, username: str, password: str, full_name: str, email: str, city: str):
+        user = self.users.find_one({"username": username})
+        if user:
+            return "Username already exists."
+
+        user = self.users.find_one({"email": email})
+
+        if user:
+            return "Email already exists."
+
+        return self.users.insert_one(
             {
                 "username": username,
                 "password": password,
                 "full_name": full_name,
                 "email": email,
                 "bio": "",
+                "reviews": [],
+                "city": city,
                 "uuid": str(uuid.uuid4())
             }
         )
@@ -29,16 +39,37 @@ class MongoController:
 
         return user
 
-    def verify_auth(self, username, password):
-        user = self.users.find_one({"username": username})
+    def edit_user(self, user_uuid: str, new_data: dict):
+        if "email" in new_data.keys():
+            new_data["email"] = str(new_data["email"]).lower()
+
+        update_result = self.users.update_one(
+            {"uuid": user_uuid},
+            {"$set": new_data}
+        )
+
+        if update_result.acknowledged:
+            return True
+        else:
+            return False
+
+    def verify_auth(self, username: str, password: str):
+        user = self.users.find_one(
+            {"username": username, "password": password}
+        )
 
         if user is None:
-            return False
+            user = self.users.find_one(
+                {"email": str(username).lower(), "password": password}
+            )
+            if user is None:
+                return False
+            elif user["password"] == password:
+                return user
+            else:
+                return user
+
         elif user["password"] != password:
             return False
         else:
             return user
-
-
-def verify_auth(username, password):
-    return username == "meow" and password == "meow"
